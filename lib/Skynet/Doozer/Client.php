34981da3052739ff16619c781f74aa639ea558c9
mikespook <mikespook@gmail.com>
2012-10-18 13:08:08 +0800

    client for doozer

--- lib/Skynet/Doozer/Client.php
+++ lib/Skynet/Doozer/Client.php
@@ -23,7 +23,8 @@ class Client {
         SO_RCVTIMEO => array("sec"=>0, "usec"=>10),
         SO_SNDTIMEO => array("sec"=>10, "usec" => 0)
     );
-    
+
+    //Create a resilient client connection to a Doozer server
     function __construct($host = '127.0.0.1', $port = '8046', $params = array()) {
         $nport = getservbyname($port, 'tcp');
         $this->_port = empty($nport) ? $port : $nport;
@@ -32,6 +33,7 @@ class Client {
         $this->_connect();
     }  
 
+    // Close this client connection to doozer
     function __destruct() {
         socket_close($this->_socket);
     }
@@ -84,23 +86,32 @@ class Client {
     private function _invoke($request) {
         $this->_write($request);
         $response = $this->_read();
-        if ($response->getErrCode() != 0) {
-            throw new Exception($response->getErrDetail());
+        $errCode =$response->getErrCode();
+        if ($errCode != 0) {
+            throw new Exception($response->getErrDetail(), $errCode);
         }
         return $response;
     }
 
+    // Shutdown this client connection to doozer
     public function close() {
         socket_shutdown($this->_socket);
     }
 
-    public function currentVersion() {
+    // Returns the current Doozer revision
+    public function currentRevision() {
         $request = new Request();
         $request->setVerb(Request\Verb::REV);
         $response = $this->_invoke($request);
         return $response->rev;
     }
 
+    /**
+     * Set a value in Doozer
+     * @path:  Path to the value to be set
+     * @value: Value to set
+     * @rev:   Revision at which to set the value
+     */
     public function set($path, $value, $rev=null) {
         $request = new Request();
         $request->setVerb(Request\Verb::SET);
@@ -111,6 +122,7 @@ class Client {
         return $response->rev;       
     }
 
+    // Return the value at the supplied path and revision
     public function get($path, $rev=null) {
         $request = new Request();
         $request->setVerb(Request\Verb::GET);
@@ -120,6 +132,12 @@ class Client {
         return $response->value;       
     }
 
+    /**
+     * Deletes the file at path if rev is greater than or equal to the file's revision.
+     * Returns nil when the file was removed
+     * Raises an exception if an attempt to remove the file and its revision
+     *  is greater than that supplied
+     */
     public function delete($path, $rev=null) {
         $request = new Request();
         $request->setVerb(Request\Verb::DEL);
@@ -128,6 +146,11 @@ class Client {
         $this->_write($request);
     }
 
+    /**
+     * Returns the directory in the supplied path
+     * Use offset to get the next
+     * returns nil if no further paths are available
+     */
     public function dir($path, $offset=0, $rev=null) {
         $request = new Request();
         $request->setVerb(Request\Verb::GETDIR);
@@ -137,4 +160,77 @@ class Client {
         $response = $this->_invoke($request);
         return $response->path;         
     }
+
+    public function stat($path, $rev=null) {
+        $request = new Request();
+        $request->setVerb(Request\Verb::STAT);
+        $request->setPath($path);
+        $request->setRev($rev);
+        $response = $this->_invoke($request);
+        return $response;    
+    }
+
+    public function access($path) {
+        $request = new Request();
+        $request->setVerb(Request\Verb::ACCESS);
+        $request->setPath($path);
+        $response = $this->_invoke($request);
+        return $response;    
+    }
+
+    /**
+     * Returns every entry in the supplied path
+     * path can also contain wildcard characters such as '*'
+     */ 
+    public function walk($path, $rev=null, $offset=0) {
+        $paths = array();
+        $rev = is_null($rev) ? $this->currentVersion() : $rev;
+        while(true) {
+            $request = new Request();
+            $request->setVerb(Request\Verb::WALK);
+            $request->setPath($path);
+            $request->setRev($rev);
+            $request->setOffset($offset);
+            $this->_write($request);
+            $response = $this->_read();
+            $errCode = $response->getErrCode();
+            if ($errCode == Response\Err::RANGE) {
+                break;
+            } else if ($errCode != 0) {
+                throw new Exception($response->getErrDetail(), $errCode);
+            }
+            $paths[] = $response;
+        }
+        return $paths;
+    }
+
+    /**
+     * Returns [Array] of hostname [String] with each string
+     * representing another Doozer server that can be connected to
+     */
+    public function doozerHosts() {
+        $hosts = array();
+        $paths = $this->walk('/ctl/node/*/addr', $this->currentRevision());
+        foreach($paths as $path) {
+            if (!empty($path.value)) {
+                $hosts[] = $path.value;
+            }
+        }
+    }
+
+    /**
+     * Wait for changes to the supplied path
+     * Returns the next change to the supplied path
+     */
+    public function wait($path, $rev = null) {
+        $rev = is_null($rev) ? $this->currentRevision() : $rev;
+        $request = new Request();
+        $request->setVerb(Request\Verb::WAIT);
+        $request->setPath($path);
+        $request->setRev($rev);
+        $response = $this->_invoke($request);
+        return $response;
+    }
+
+
 }
