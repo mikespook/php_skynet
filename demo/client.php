a047cd2a0148dd23bb78a772a4df620d7932900e
mikespook <mikespook@gmail.com>
2012-10-23 18:14:05 +0800

    add a client demo

---
+++ demo/client.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT', '../lib/');
+require(ROOT . 'Skynet/Doozer/Exception.php');
+require(ROOT . 'Skynet/Doozer/Client.php');
+require(ROOT . 'Skynet/Registry.php');
+require(ROOT . 'Skynet/Cache/File.php');
+require(ROOT . 'Skynet/Client.php');
+
+$client =  new \Skynet\Client();
